bd889cd78ad4385280025a02d7134ae1ca6b3fbd
Ben Awad <benawad97@gmail.com>
2019-06-29 21:09:11 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import Cookie from "js-cookie";
+import { parseCookies } from "../lib/parseCookies";
+
+const Index = ({ initialRememberValue }) => {
+  const [rememberMe, setRememberMe] = useState(() =>
+    JSON.parse(initialRememberValue)
+  );
+
+  useEffect(() => {
+    Cookie.set("rememberMe", JSON.stringify(rememberMe));
+  }, [rememberMe]);
+
+  return (
+    <div>
+      remember me
+      <input
+        type="checkbox"
+        value={rememberMe}
+        checked={rememberMe}
+        onChange={e => setRememberMe(e.target.checked)}
+      />
+    </div>
+  );
+};
+
+Index.getInitialProps = ({ req }) => {
+  const cookies = parseCookies(req);
+
+  return {
+    initialRememberValue: cookies.rememberMe
+  };
+};
+
+export default Index;
